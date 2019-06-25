cc76303ca70af0e010879c515f54f6fe1c445237
huaqiao.chen <huaqiao.chen@biz-united.com.cn>
2019-06-25 16:22:03 +0800

    edit block

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.css';
+
+const Block: React.FC = () => {
+  return (
+    <div className={styles.normal}>
+      <h1> 这是一个远端的物料市场</h1>
+    </div>
+  );
+}
+
+export default Block;
